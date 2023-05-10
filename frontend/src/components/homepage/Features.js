0a30285427452f4a0ce2b0e261675727e09959b1
mong0019 <muthusamicurry@gmail.com>
2023-05-10 13:25:13 +1000

    Added images and new feature descriptions and upcoming features in home page

--- frontend/src/components/homepage/Features.js
+++ frontend/src/components/homepage/Features.js
@@ -7,9 +7,9 @@ function Features() {
         <h1 className="text-white text-center capitalize ">
           Save your online world
         </h1>
-        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-6 mt-16   ">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-6 mt-16">
           <div className="rounded-2xl overflow-hidden bg-white">
-            <img className="w-full" src="/images/test.jpg" alt="" />
+            <img className="w-full h-4/6 object-cover" src="/images/category.PNG" alt="" />
             <div className="w-full px-3 py-6 ">
               <h1 style={{ marginBottom: "0" }} className="text-2xl font-bold">
                 {" "}
@@ -17,26 +17,25 @@ function Features() {
               </h1>
               <h6 style={{ marginBottom: "0" }}>
                 {" "}
-                Sort your favourite links like your favourite korean restaurants and share them!
+                Sort your favourite links like your favourite pizza restaurants and share them by just sending the category link!
               </h6>
             </div>
           </div>
           <div className="rounded-2xl overflow-hidden bg-white">
-            <img className="w-full" src="/images/test.jpg" alt="" />
+            <img className="w-full h-4/6 object-cover" src="/images/listentry.PNG" alt="" />
             <div className="w-full px-3 py-6 ">
               <h1 style={{ marginBottom: "0" }} className="text-2xl font-bold">
                 {" "}
                 Quick and simple entries
               </h1>
               <h6 style={{ marginBottom: "0" }}>
-           
                 Create and name each webpage entry and just paste the link of your
               webpage
               </h6>
             </div>
           </div>
           <div className="rounded-2xl overflow-hidden bg-white">
-            <img className="w-full" src="/images/test.jpg" alt="" />
+            <img className="w-full h-4/6" src="/images/collection1.PNG" alt="" />
             <div className="w-full px-3 py-6 ">
               <h1 style={{ marginBottom: "0" }} className="text-2xl font-bold">
               Multiple Collections
@@ -47,7 +46,6 @@ function Features() {
               </h6>
             </div>
           </div>
-   
         </div>
       </div>
     </section>
--- frontend/src/components/homepage/Learn.jsx
+++ frontend/src/components/homepage/Learn.jsx
@@ -5,18 +5,19 @@ function Learn(){
     <section style={{background:"rgba(0,0,0,0.9)"}} className=" bg-gray-900 w-full py-20 flex items-center justify-center">
     <div className="container">
       <h1 className="text-white text-center ">
-        Learn More About Us
+        Future features
       </h1>
       <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-6 mt-16   ">
         <div className="rounded-2xl overflow-hidden ">
           <img className="w-full" src="/images/l1.png" alt="" />
           <div className="w-full px-3 py-6 ">
             <h1 style={{ marginBottom: "0" }} className="text-2xl text-white font-bold">
-             Our Goal
+             Mobile App
             </h1>
             <p className='text-white font-light' style={{ marginBottom: "0" }}>
               {" "}
-             Create a central hub of your internet and revolutionise how people save their bookmarks
+             Creating a mobile app to allow users to access their net diary from their phone and easily save links
+             through apps like tiktok and instagram
             </p>
           </div>
         </div>
@@ -24,11 +25,12 @@ function Learn(){
           <img className="w-full" src="/images/l1.png" alt="" />
           <div className="w-full px-3 py-6 ">
             <h1 style={{ marginBottom: "0" }} className="text-2xl text-white font-bold">
-             Who we are
+             Faster saving through AI 
             </h1>
             <p className='text-white font-light' style={{ marginBottom: "0" }}>
               {" "}
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ratione.
+             Integrating ChatGpt to automatically categorise your new links when saving or insert them into existing
+             categories
             </p>
           </div>
         </div>
@@ -36,16 +38,15 @@ function Learn(){
           <img className="w-full" src="/images/l1.png" alt="" />
           <div className="w-full px-3 py-6 ">
             <h1 style={{ marginBottom: "0" }} className="text-2xl text-white font-bold">
-              What We do
+              Advanced Suggestions
             </h1>
             <p className='text-white font-light' style={{ marginBottom: "0" }}>
               {" "}
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ratione.
+             Suggestion functionality that can be toggled on or off for specific categories. These suggestions will 
+             find links similar to the ones you have saved.
             </p>
           </div>
         </div>
-
- 
       </div>
     </div>
   </section>
